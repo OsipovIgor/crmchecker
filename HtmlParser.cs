be696e760fc66f084dd90a14acaf01e3af77aad6
OsipovIgor <9533121456@mail.ru>
2015-08-19 14:55:30 +0300

    Механизм парсинга сайта вынес в отдельный класс

---
+++ HtmlParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace CrmParser
+{
+    /// <summary>
+    /// Класс для парсинга сайта
+    /// </summary>
+    public class HtmlParser
+    {
+        /// <summary>
+        /// Метод возвращает список значений (кол-во звонков, пропущеных и т.д.)
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetHeaderData()
+        {
+            string request = Download("http://panel.support.voip.astralnalog.ru/table?queue=Q1co-crm");
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(request);
+            IEnumerable<HtmlNode> innerText = document.GetElementbyId("header").Descendants("tr").Select(x => x.Elements("td")).FirstOrDefault();
+            List<string> result = (from t in innerText select t.InnerHtml).ToList();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Метод возвращает страницу в виде HTML
+        /// </summary>
+        /// <param name="uri">URL страницы</param>
+        /// <returns></returns>
+        private string Download(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(uri);
+            request.Method = "GET";
+            String result = String.Empty;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                result = reader.ReadToEnd();
+                reader.Close();
+                dataStream.Close();
+            }
+            return result;
+        }
+    }
+}
--- MainForm.cs
+++ MainForm.cs
@@ -40,40 +40,14 @@ namespace CrmParser
 
         void timer1_Tick(object sender, EventArgs e)
         {
-            var res = Get1CTime();
+            HtmlParser htmlParser = new HtmlParser();
+            var res = htmlParser.GetHeaderData();
             labelWait1C.Text = res[3];
             labelWait1C.ForeColor = !res[3].Contains("WAIT 0") ? Color.Red : Color.Black;
             labelLost1C.Text = res[6];
             int lastIndex = res[2].LastIndexOf('>');
 
             labelFree1C.Text = string.Format("FREE: {0}",res[2].Substring(lastIndex+1)); 
-        }
-
-        public static List<string> Get1CTime()
-        {
-            string request = Download("http://panel.support.voip.astralnalog.ru/table?queue=Q1co-crm");
-            HtmlDocument document = new HtmlDocument();
-            document.LoadHtml(request);
-            IEnumerable<HtmlNode> innerText = document.GetElementbyId("header").Descendants("tr").Select(x => x.Elements("td")).FirstOrDefault();
-            List<string> result = (from t in innerText select t.InnerHtml).ToList();
-
-            return result;
-        }
-
-        public static string Download(string uri)
-        {
-            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(uri);
-            request.Method = "GET";
-            String test = String.Empty;
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            {
-                Stream dataStream = response.GetResponseStream();
-                StreamReader reader = new StreamReader(dataStream);
-                test = reader.ReadToEnd();
-                reader.Close();
-                dataStream.Close();
-            }
-            return test;
         }
 
         private void btnClose_Click(object sender, EventArgs e)
